c541f857f477a0c4852bd5e761a5725762144bc1
Navi Gamer <97757766+navigamer123@users.noreply.github.com>
2025-07-03 22:08:26 +0300

    Delete ArduinoFinal directory

--- ArduinoFinal/include/loadProgram.h
+++
@@ -1,8 +0,0 @@
-#ifndef modbusFiles_h
-#define modbusFiles_h
-
-extern void loopFilesFromDisp();
-extern String ReadFileName(int FileIndex);
-extern int ReadFileRepetition(int FileIndex);
-extern uint16_t numFiles;
-#endif
